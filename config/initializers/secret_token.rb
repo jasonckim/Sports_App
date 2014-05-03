058fcc2cd3286d51e0f873dddcab3576252596f3
Jason <jasonckim10@gmail.com>
2014-05-03 15:09:19 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SportsApp::Application.config.secret_key_base = 'b2d058c31df986a8b02dfccdad7912ad23a9b1f78c18f71e89113eb651ccd288e423db8828aa3cba4797d74034575edce9a3f53654ea0c340110e842ea0ef4f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SportsApp::Application.config.session_store :cookie_store, key: '_Sports_App_session'
